ff89953bf9d769e70cb4a9c5f62a185f6d83aa94
Kevin Delugan <bisumto@gmail.com>
2019-06-03 23:56:19 +0200

    Moved include js functions

--- js/scripts.js
+++ js/scripts.js
@@ -24,32 +24,11 @@ $(window).on("load", function() {
 
         element = document.getElementById("nav-link-" + pathname);
         element.classList.add("active");
+
+        includeHTML()
     }
 
     if(document.getElementById('footerPush') != null && document.body.clientHeight - document.getElementById('mainNavbar').clientHeight - document.getElementById('mainFooter').clientHeight < $(window).height()) {
         document.getElementById('footerPush').style.height = ($(window).height() - document.body.clientHeight) + 'px';
     }
 });
-
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-            }
-        }      
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        return;
-        }
-    }
-};
