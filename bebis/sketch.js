fc729822d774efe9e89105fe230b69c0274dcbb6
BisUmTo <31419791+BisUmTo@users.noreply.github.com>
2019-12-05 17:36:40 +0100

    Restore

--- bebis/sketch.js
+++ bebis/sketch.js
@@ -348,11 +348,12 @@ function touchEnded(){
 }
 
 function setup() {
-	PIECES_POS={x:windowWidth/6,dx:windowWidth/4,y:windowHeight-150};
-        GRID_POS={x:10,y:0.8*windowWidth+30,dx:windowWidth-40,dy:windowWidth-40};
-        PREVIEW_POS={x:0.1*windowWidth,y:20,dx:0.8*windowWidth-40,dy:0.8*windowWidth-40};
-	createCanvas(windowWidth, windowHeight).center('horizontal');
-	solution_from_image(randomInt(IMAGES.length));
+	//PIECES_POS={x:windowWidth/6,dx:windowWidth/4,y:windowHeight-150};
+        //GRID_POS={x:10,y:0.8*windowWidth+30,dx:windowWidth-40,dy:windowWidth-40};
+        //PREVIEW_POS={x:0.1*windowWidth,y:20,dx:0.8*windowWidth-40,dy:0.8*windowWidth-40};
+	//createCanvas(windowWidth, windowHeight).center('horizontal');
+	createCanvas(800, 1220).center('horizontal');
+        solution_from_image(randomInt(IMAGES.length));
 	initialize_grid(); 
 	PIECES = new_pieces();
 }
