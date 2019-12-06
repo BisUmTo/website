d15f3d49419b6a3ec12a84ba80781ee1c6771232
BisUmTo <31419791+BisUmTo@users.noreply.github.com>
2019-12-06 13:02:20 +0100

    Update sketch.js

--- bebis/sketch.js
+++ bebis/sketch.js
@@ -237,8 +237,8 @@ function new_pieces(){
 function mousePressed() {
 	let ret = true;
 	for (let i=0;i<PIECES.length;i++){
-                let dx = (PIECES_POS.hb-PIECES[i].cell.x)/2;
-                let dy = (PIECES_POS.hb-PIECES[i].cell.y)/2;
+                let dx = (PIECES_POS.x)/4;
+                let dy = (PIECES_POS.x)/4;
 		if (mouseX > PIECES[i].hitbox.x-dx && mouseX < PIECES[i].hitbox.fx+dx && mouseY > PIECES[i].hitbox.y-dy && mouseY < PIECES[i].hitbox.fy+dy) {
 			PIECES[i].drag.dragging=true;
 			PIECES[i].drag.offx = PIECES[i].hitbox.x-mouseX;
@@ -414,8 +414,7 @@ function layout(){
 			x:(windowWidth-(ww-2*mr))/2+(ww-2*mr)/6,
 			y:wh-1.5/DIM.h*(ww-2*mr)-mr,
 			dx:(ww-2*mr)/3,
-			dy:0,
-                        hb:1.5/DIM.h*(ww-2*mr)-mr
+			dy:0
 		};
 	} else {
 		let mr = 0.01*windowHeight;
@@ -439,8 +438,7 @@ function layout(){
 			y:(windowHeight-(ww-2*mr))/2+(ww-2*mr)/6,
 			x:wh-1.5/DIM.h*(ww-2*mr)-mr,
 			dy:(ww-2*mr)/3,
-			dx:0,
-                        hb:1.5/DIM.h*(ww-2*mr)-mr
+			dx:0
 		};
 	}
 }
