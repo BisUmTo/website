e3b2bb7c3b6422fe53efd8e8ae705a8a13481759
Kevin Delugan <bisumto@gmail.com>
2020-04-28 11:30:33 +0200

    Blank target - RedCraft2

--- js/pages/redpack.js
+++ js/pages/redpack.js
@@ -1,46 +1,34 @@
-/**
- * Reset the message.
- */
-function resetMessage () {
+function resetMessage() {
     $("#result")
-    .removeClass()
-    .text("");
+        .removeClass()
+        .text("");
 }
-/**
- * show a successful message.
- * @param {String} text the text to show.
- */
+
 function showMessage(text) {
     resetMessage();
     $("#result")
-    .addClass("alert alert-success")
-    .text(text);
+        .addClass("alert alert-success")
+        .text(text);
 }
-/**
- * show an error message.
- * @param {String} text the text to show.
- */
+
 function showError(text) {
     resetMessage();
     $("#result")
-    .addClass("alert alert-danger")
-    .text(text);
-}
-/**
- * Update the progress bar.
- * @param {Integer} percent the current percent
- */
-function updatePercent(percent) {
-    $("#progress_bar").removeClass("hide")
-    .find(".progress-bar")
-    .attr("aria-valuenow", percent)
-    .css({
-        width : percent + "%"
-    });
+        .addClass("alert alert-danger")
+        .text(text);
 }
 
-if(!JSZip.support.blob) {
-    showError("This demo works only with a recent browser !");
+function updatePercent(percent) {
+    $("#progress_bar").removeClass("hide")
+        .find(".progress-bar")
+        .attr("aria-valuenow", percent)
+        .css({
+            width: percent + "%"
+        });
+}
+
+if (!JSZip.support.blob) {
+    showError("Purtroppo il tuo browser non è aggiornato... Prova ad accedere con un nuovo browser!");
 }
 
 var Promise = window.Promise;
@@ -48,56 +36,105 @@ if (!Promise) {
     Promise = JSZip.external.Promise;
 }
 
-/**
- * Fetch the content and return the associated promise.
- * @param {String} url the url of the content to fetch.
- * @return {Promise} the promise containing the data.
- */
 function urlToPromise(url) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         JSZipUtils.getBinaryContent(url, function (err, data) {
-            if(err) {
+            if (err) {
                 reject(err);
             } else {
                 resolve(data);
             }
         });
     });
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 var $form = $("#download_form").on("submit", function () {
-
     resetMessage();
-
     var zip = new JSZip();
 
-    // find every checked item
+    var promises = [];
+    var ticks = [];
+    var loads = [];
+
     $(this).find(":checked").each(function () {
         var $this = $(this);
         var url = $this.data("url");
-        var filename = url.replace(/.*\//g, "");
-        zip.file(filename, urlToPromise(url), {binary:true});
-    
+        promises.push(
+            new Promise((resolve, reject) => {
+                var msg = "Download: " + url;
+                showMessage(msg);
+                new Promise((res, rej) => {
+                        JSZipUtils.getBinaryContent(url, (err, data) => {
+                            if (err) {
+                                rej(err);
+                            } else {
+                                res(data);
+                            }
+                        })
+                    }).then(JSZip.loadAsync)
+                    .then((zipToMerge) => {
+                        var subpromises = [];
+                        zipToMerge.forEach((relativePath, file) => {
+                            subpromises.push(
+                                new Promise((res, rej) => {
+                                    if (relativePath == 'data/minecraft/tags/functions/load.json') {
+                                        zipToMerge.file(relativePath).async("string").then((value) => {
+                                            JSON.parse(value).values.forEach((el) => {
+                                                loads.push(el);
+                                            });
+                                            res();
+                                        });
+                                    } else if (relativePath == 'data/minecraft/tags/functions/tick.json') {
+                                        zipToMerge.file(relativePath).async("string").then((value) => {
+                                            JSON.parse(value).values.forEach((el) => {
+                                                ticks.push(el);
+                                            });
+                                            res();
+                                        });
+                                    } else if (!file.dir) {
+                                        strpromise = zipToMerge.file(relativePath).async("string");
+                                        zip.file(relativePath, strpromise);
+                                        strpromise.then(() => {
+                                            res();
+                                        });
+                                    } else {
+                                        res();
+                                    }
+                                })
+                            );
+                        });
+                        Promise.all(subpromises).then(() => {
+                            resolve()
+                        });
+                    })
+            })
+        )
     });
 
-    // when everything has been downloaded, we can trigger the dl
-    zip.generateAsync({type:"blob"}, function updateCallback(metadata) {
-        var msg = "progression : " + metadata.percent.toFixed(2) + " %";
-        if(metadata.currentFile) {
-            msg += ", current file = " + metadata.currentFile;
+    Promise.all(promises).then(() => {
+        zip.file('data/minecraft/tags/functions/tick.json', '{"values":' + JSON.stringify(ticks) + '}');
+        zip.file('data/minecraft/tags/functions/load.json', '{"values":' + JSON.stringify(loads) + '}');
+    }).then(
+        function () {
+            zip.generateAsync({
+                type: "blob"
+            }, function updateCallback(metadata) {
+                var msg = "Compressione : " + metadata.percent.toFixed(2) + " %";
+                if (metadata.currentFile) {
+                    msg += ", " + metadata.currentFile;
+                }
+                showMessage(msg);
+                updatePercent(metadata.percent | 0);
+            }).then((blob) => {
+                saveAs(blob, "RedPack.zip");
+                showMessage("RedPack generato con successo!");
+            }, (e) => showError(e));
         }
-        showMessage(msg);
-        updatePercent(metadata.percent|0);
-    })
-    .then(function callback(blob) {
-
-        // see FileSaver.js
-        saveAs(blob, "example.zip");
-
-        showMessage("done !");
-    }, function (e) {
-        showError(e);
-    });
+    );
 
     return false;
 });
