7a3410374c00c45d3c125d62c8136783bfc8a23a
Kevin Delugan <bisumto@gmail.com>
2020-10-03 14:57:51 +0200

    /color FLOAT

--- js/pages/colore.js
+++ js/pages/colore.js
@@ -1,9 +1,11 @@
-const gravity=0.04;
-const damping=0.98;
-
 function calculate() {
     var color = document.getElementById('colorpicker').value;
     document.getElementById('coloreHEX').innerHTML = color;
     document.getElementById('coloreDEC').innerHTML = parseInt(color.replace('#','0x'));
 
+    document.getElementById('coloreFLOAT_R').innerHTML = parseFloat((parseInt(color.replace('#','0x')) & 16711680)/(255*65536)).toFixed(5);
+    document.getElementById('coloreFLOAT_G').innerHTML = parseFloat((parseInt(color.replace('#','0x')) & 65280)/(255*256)).toFixed(5);
+    document.getElementById('coloreFLOAT_B').innerHTML = parseFloat((parseInt(color.replace('#','0x')) & 255)/255).toFixed(5);
 }
+
+calculate()
