449495f91bc6b776ff634e86ae9dbe5e30afe600
Kevin Delugan <bisumto@gmail.com>
2020-11-10 15:37:43 +0100

    No Cookie YouTube

--- js/pages/home.js
+++ js/pages/home.js
@@ -3,6 +3,6 @@ $(window).on('load', function() {
     $.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D' + channelID, function(data) {
         var link = data.items[0].link;
         var id = link.substr(link.indexOf('=')+1);
-        $('#youtubeVideo').attr('src','https://youtube.com/embed/' + id + '?controls=0&showinfo=0&rel=0');
+        $('#youtubeVideo').attr('src',(!localStorage.getItem('cookieconsent')? 'https://www.youtube-nocookie.com/embed/' : 'https://youtube.com/embed/') + id + '?controls=0&showinfo=0&rel=0');
     });
 });
