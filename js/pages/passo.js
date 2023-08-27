3894d0a3a3eb607330b8c536b8aafefeaccf82af
BisUmTo <bisumto@gmail.com>
2023-08-27 21:22:01 +0200

    /passo

---
+++ js/pages/passo.js
@@ -0,0 +1,10 @@
+function calculate() {
+    var minuti = parseInt(document.getElementById('minuti').value) || 0;
+    var secondi = parseInt(document.getElementById('secondi').value) || 0;
+    var tempo = (secondi / 60 + minuti);
+    console.log(tempo)
+    var velocita = tempo == 0 ? "flash" : (60 / tempo).toFixed(2);
+    document.getElementById('velocita').innerHTML = velocita;
+}
+
+calculate()
