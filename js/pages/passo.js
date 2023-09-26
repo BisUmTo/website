b33c0be9e93e0b06b0b53745b19c5435d1bc5e7a
BisUmTo <bisumto@gmail.com>
2023-09-26 11:07:10 +0200

    Riorganizzati caricamenti dinamici
    - Centralizzata l'inclusione delle componenti nel file script.js
    - Spostato l'aggiornamento dinamico dell'anno in script.js
    - Aggiornati i path nella navigazione per essere percorsi assoluti
    - Migliorato addattamento iframe nella pagina redcraft2
    - Impostato il background della pagina a grigio di default, per evitare brutti effetti grafici durante lo scorrimento della pagina oltre al limite previsto
    - Rimossi log di debug in console
    - Automatizzata selezione pagina nel menu corrente in base al pathname

--- js/pages/passo.js
+++ js/pages/passo.js
@@ -2,7 +2,6 @@ function calculate() {
     var minuti = parseInt(document.getElementById('minuti').value) || 0;
     var secondi = parseInt(document.getElementById('secondi').value) || 0;
     var tempo = (secondi / 60 + minuti);
-    console.log(tempo)
     var velocita = tempo == 0 ? "flash" : (60 / tempo).toFixed(2);
     document.getElementById('velocita').innerHTML = velocita;
 }
--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,41 @@ $(document).on('click', function() {
         });
     }
 })();
+
+let promises = [];
+function includeHTML() {
+    const z = document.getElementsByTagName("*");
+    const loadPromise = (e, file) => new Promise(function(resolve, reject) {
+        $(e).load(file, resolve);
+    }); 
+    for(let i=0; i<z.length; i++) {
+        const e = z[i];
+        let file = e.getAttribute("include-html");
+        if(file) {
+            promises.push(loadPromise(e, file));
+            e.removeAttribute("include-html");
+            includeHTML();
+            return;
+        }
+    }
+
+    Promise.all(promises).then(() => {
+        console.log("All promises resolved!");
+        const pathname = window.location.pathname.split("/");
+        const pagina = pathname[pathname.length - 2] || 'home';
+        const navLink = document.getElementById("nav-link-" + pagina);
+        if (navLink != null) navLink.classList.add("active");
+        console.log(navLink,pagina)
+        
+        if(document.getElementById('footerPush') != null && document.body.clientHeight - document.getElementById('mainNavbar').clientHeight - document.getElementById('mainFooter').clientHeight < $(window).height()) {
+            document.getElementById('footerPush').style.height = ($(window).height() - document.body.clientHeight) + 'px';
+        }
+
+        const currentYear = new Date().getFullYear();
+        let spanYear = document.getElementsByClassName("currentYear");
+        for(let i = 0; i < spanYear.length; i++)
+            spanYear[i].innerHTML = currentYear;
+    });
+};
+
+window.addEventListener('load', includeHTML);
